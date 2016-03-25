9e498b16caecbcc38afbf9ae87d6afb59af2abcd
Carlos OmdelC <carlosomdelc_93@hotmail.com>
2016-03-25 01:06:42 -0600

    Actualizaciones

---
+++ app/app.js
@@ -0,0 +1,16 @@
+angular.module('SOweb', [
+  'ui.router'
+]).
+config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+	$urlRouterProvider.otherwise('home');
+	$stateProvider.
+	state('home', {
+		url: '/home',
+    views: {
+      '': { /* no caracter ni espacios coloca esta view como la default */
+        templateUrl: 'views/index.html',
+        controller: 'homeController'
+      }
+    }
+	});
+}]);
